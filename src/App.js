c15d90d03f3fbc3345e0dd699c5afdf9dc506bf9
Juan Sanchez <juansan1993@hotmail.com>
2023-02-25 08:59:44 -0500

    Update background style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>React App</h1>
+    <div className="app">
+      <h1 className="app__header">To do list</h1>
+      <main className="app__content"></main>
+      <footer className="footer">
+        <ul className="footer__ul">
+          <li className="footer__li">All</li>
+          <li className="footer__li">All</li>
+          <li className="footer__li">All</li>
+          <li className="footer__li">All</li>
+          <li className="footer__li">All</li>
+        </ul>
+      </footer>
     </div>
   );
 }
